99167459fcd8bd92d45fd85f9c11d4811d3485e1
Anuj Kumar <99342265+AnujIstwal@users.noreply.github.com>
2024-08-01 16:55:27 +0530

    Update notification.js

--- server/routes/notification.js
+++ server/routes/notification.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Notification = require("../models/Notification");
+const Notification = require("../models/notification");
 
 router.get("/:id", async (req, res) => {
     try {
